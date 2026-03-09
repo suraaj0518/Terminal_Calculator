4b999c0bd3a3934c92be49151e00df0987e6bcf7
suraaj0518 <suraajmthakare05@gmail.com>
2026-03-09 18:52:54 +0530

    Refactor calculator variable names and output formatting
    Refactor calculator to use 'x' and 'y' instead of 'num1' and 'num2'. Format output with commas for better readability.

--- Calculator.py
+++ Calculator.py
@@ -1,14 +1,14 @@
 while True:
-    num1 = float(input("Enter your first operation: "))
+    x = float(input("Enter your first operation: "))
     operation = (input("Enter your operation(+,-,*,/): "))
-    num2 = float(input("Enter your second operation: ")) 
+    y = float(input("Enter your second operation: ")) 
     print("= ", end="")
 
     if operation == "+":
-        print(num1 + num2)
+        print(f"{x + y:,}")
     if operation == "-":
-        print(num1 - num2)
+        print(f"{x - y:,}")
     if operation == "*":
-        print(num1 * num2)
+        print(f"{x * y:,}")
     if  operation == "/":
-        print(num1 / num2)
+        print(f"{x / y:,}")
